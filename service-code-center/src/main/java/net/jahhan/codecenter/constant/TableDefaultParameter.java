613e0b59bbe3b5cdb6f927bdf028e1ffc40f5fe4
nince <41284688@qq.com>
2018-01-20 12:21:18 +0800

    漏提交

--- service-code-center/src/main/java/net/jahhan/codecenter/constant/TableDefaultParameter.java
+++ service-code-center/src/main/java/net/jahhan/codecenter/constant/TableDefaultParameter.java
@@ -3,9 +3,11 @@ package net.jahhan.codecenter.constant;
 import net.jahhan.codecenter.bean.ParaEntity;
 
 public class TableDefaultParameter {
-	public static final ParaEntity LOCALCACHE = new ParaEntity("localCache", "true");
+	public static final ParaEntity LOCAL_CACHE = new ParaEntity("localCache", "true");
 
-	public static final ParaEntity REDISCACHE = new ParaEntity("redisCache", "true");
+	public static final ParaEntity REDIS_CACHE = new ParaEntity("redisCache", "true");
 
-	public static final ParaEntity METHODEXTENTION = new ParaEntity("methodExtention", "false");
+	public static final ParaEntity METHOD_EXTENTION = new ParaEntity("methodExtention", "false");
+	
+	public static final ParaEntity CACHE_EXPIRE_TIME = new ParaEntity("cacheExpireTime", "0");
 }
